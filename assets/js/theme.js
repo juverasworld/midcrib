091c095e6155dc396ef15fb3ba8848a31c4657bd
Vee <ifebuchejulia@gmail.com>
2025-08-16 21:04:05 +0100

    first commit

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -52,21 +52,21 @@
     new WOW().init();
 
     /*faqs*/
-    $(document).ready(function() {
-  $(".faqs-container .faq-singular:first-child").addClass("active").children(".faq-answer").slideDown();//Remove this line if you dont want the first item to be opened automatically.
-  $(".faq-question").on("click", function(){
-    if( $(this).parent().hasClass("active") ){
-      $(this).next().slideUp();
-      $(this).parent().removeClass("active");
-    }
-    else{
-      $(".faq-answer").slideUp();
-      $(".faq-singular").removeClass("active");
-      $(this).parent().addClass("active");
-      $(this).next().slideDown();
-    }
-  });
-});
+//     $(document).ready(function() {
+//   $(".faqs-container .faq-singular:first-child").addClass("active").children(".faq-answer").slideDown();//Remove this line if you dont want the first item to be opened automatically.
+//   $(".faq-question").on("click", function(){
+//     if( $(this).parent().hasClass("active") ){
+//       $(this).next().slideUp();
+//       $(this).parent().removeClass("active");
+//     }
+//     else{
+//       $(".faq-answer").slideUp();
+//       $(".faq-singular").removeClass("active");
+//       $(this).parent().addClass("active");
+//       $(this).next().slideDown();
+//     }
+//   });
+// });
 
 
     /* Portfolio Isotope  */
